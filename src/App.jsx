aabc6e8ae4c2a84f5c1d8e031db5a7e508563734
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-31 01:13:20 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import {
   DigiExpandableFaq,
-  DigiExpandableFaqItem, DigiInfoCard, DigiInfoCardMulti,
+  DigiExpandableFaqItem,
+  DigiInfoCard,
+  DigiInfoCardMulti,
   DigiInfoCardMultiContainer,
-  DigiLayoutContainer
+  DigiLayoutBlock,
+  DigiLayoutContainer,
+  DigiLinkButton,
+  DigiMediaImage,
+  DigiTypography
 } from "@digi/arbetsformedlingen-react";
 
 import {
-  ExpandableFaqVariation, InfoCardHeadingLevel, InfoCardMultiHeadingLevel,
-  InfoCardMultiType, InfoCardType, InfoCardVariation
+  ExpandableFaqVariation,
+  InfoCardHeadingLevel,
+  InfoCardMultiHeadingLevel,
+  InfoCardMultiType,
+  InfoCardType,
+  InfoCardVariation,
+  LayoutBlockVariation,
+  LinkButtonSize,
+  LinkButtonVariation,
+  TypographyVariation,
 } from "@digi/arbetsformedlingen";
 import "./App.css";
 
@@ -15,50 +29,87 @@ function App() {
   return (
     <div className="App">
       <DigiLayoutContainer afVerticalPadding className="white">
-        <h1 className="rubrik">Hej!</h1>
-        <div className="flex space">
-          <DigiInfoCard
-            afHeading="Jag är ett infokort"
-            afHeadingLevel={InfoCardHeadingLevel.H2}
-            afType={InfoCardType.TIP}
-            afLinkHref="Frivillig länk"
-            afLinkText="Frivillig länktext"
-            afVariation={InfoCardVariation.PRIMARY}
-          >
-            <p>Jag heter Mimmi och söker Frontend praktik hos er :-D</p>
-          </DigiInfoCard>
-
+        <DigiLayoutBlock afVariation={LayoutBlockVariation.PROFILE}>
+          <div className="one">
+            <DigiTypography>
+              <DigiTypography afVariation={TypographyVariation.LARGE}>
+                <h1>Hej!</h1>
+                <p className="bold">
+                  Jag heter Mimmi och söker Frontend praktik hos er!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, test link adipiscing elit. Nullam
+                  dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc
+                  iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi,
+                  imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent
+                  mattis, massa quis luctus fermentum, turpis mi volutpat justo,
+                </p>
+                <div className="flex buttongroup">
+                  <DigiLinkButton
+                    afHref="#"
+                    afSize={LinkButtonSize.MEDIUM}
+                    afVariation={LinkButtonVariation.PRIMARY_ALT}
+                    af-hide-icon={true}
+                    className="knapp"
+                  >
+                    Detta är en länk
+                  </DigiLinkButton>
+                  <DigiLinkButton
+                    afHref="#"
+                    afSize={LinkButtonSize.MEDIUM}
+                    afVariation={LinkButtonVariation.SECONDARY_ALT}
+                    af-hide-icon={true}
+                  >
+                    Här är en länk
+                  </DigiLinkButton>
+                </div>
+              </DigiTypography>
+            </DigiTypography>
+            <DigiMediaImage
+                afUnlazy
+                afHeight="330"
+                afWidth="330"
+                afSrc="https://cdn.discordapp.com/attachments/978099866343440416/1091125463411326996/mim1.png"
+                afAlt="teckning på mimmi"
+              ></DigiMediaImage>
+          </div>
+        </DigiLayoutBlock>
+        <DigiLayoutContainer afVerticalPadding className="white">
+        <DigiInfoCard
+              afHeading="Det bästa djuret är katter!"
+              afHeadingLevel={InfoCardHeadingLevel.H2}
+              afType={InfoCardType.TIP}
+              afLinkHref="Frivillig länk"
+              afLinkText="Frivillig länktext"
+              afVariation={InfoCardVariation.PRIMARY}
+            >
+              <span>Här är lite information om katter. Dom gillar att jaga och leka med garn.  </span>
+            </DigiInfoCard>
+          <div className="">
+            <DigiExpandableFaq
+              afHeading="Vanliga frågor"
+              afVariation={ExpandableFaqVariation.PRIMARY}
+            >
+              <DigiExpandableFaqItem afHeading="Vad studerar du?">
+                <span>
+                  Jag studerar just nu till frontend developer på
+                  Medieinstitutet.
+                  <digi-icon-comunication-graduate></digi-icon-comunication-graduate>
+                </span>
+              </DigiExpandableFaqItem>
+              <DigiExpandableFaqItem afHeading="När ska du ut på LIA?">
+                <p>Jag ska ut på min första LIA i September i år.</p>
+              </DigiExpandableFaqItem>
+              <DigiExpandableFaqItem afHeading="Vad gillar du att göra?">
+                <span>
+                  På fritiden gillar jag att teckna och måla, spela spel och
+                  skapa mina egna spel.
+                </span>
+              </DigiExpandableFaqItem>
           
-          <DigiExpandableFaq
-            afHeading="Vanliga frågor"
-            afVariation={ExpandableFaqVariation.PRIMARY}
-          >
-            <DigiExpandableFaqItem afHeading="Fråga 1">
-              <p>
-                Svar 1 - Reprehenderit ea nulla dolore non fugiat aute.
-                Reprehenderit velit velit non eiusmod. Voluptate commodo
-                consectetur ullamco velit minim quis. Reprehenderit consectetur
-                nisi officia est fugiat id anim incididunt qui eiusmod.
-              </p>
-            </DigiExpandableFaqItem>
-            <DigiExpandableFaqItem afHeading="Fråga 2">
-              <p>
-                Svar 2 - In veniam nostrud esse velit velit incididunt ullamco
-                non adipisicing. Dolore sint quis aute nostrud. Pariatur commodo
-                ullamco dolor nulla ut. Consequat amet velit veniam ipsum
-                reprehenderit tempor. Eu labore enim officia anim laboris magna
-                eu eu ad. Pariatur voluptate sint nisi ut nulla.
-              </p>
-            </DigiExpandableFaqItem>
-            <DigiExpandableFaqItem afHeading="Fråga 3">
-              <p>
-                Svar 3 - Officia laboris commodo quis ex nisi cupidatat officia
-                dolore est. Ad dolore exercitation sunt deserunt Lorem do esse
-                reprehenderit ex non. Proident occaecat elit enim ullamco.
-              </p>
-            </DigiExpandableFaqItem>
-          </DigiExpandableFaq>
-        </div>
+            </DigiExpandableFaq>
+          </div>
+        </DigiLayoutContainer>
 
         <DigiInfoCardMultiContainer>
           <DigiInfoCardMulti
@@ -67,9 +118,9 @@ function App() {
             afType={InfoCardMultiType.RELATED}
             afLinkHref="/"
           >
-            <p>
+            <span>
               Jag älskar att lära mig nya saker och är öppen för nya tekniker.
-            </p>
+            </span>
           </DigiInfoCardMulti>
           <DigiInfoCardMulti
             afHeading="Har jag erfarenhet av att jobba i olika projekt och team ?"
@@ -77,18 +128,17 @@ function App() {
             afType={InfoCardMultiType.RELATED}
             afLinkHref="/"
           >
-            <p>Bra på att samarbeta och jobba i olika team!</p>
+            <span>Bra på att samarbeta och jobba i olika team!</span>
           </DigiInfoCardMulti>
           <DigiInfoCardMulti
-            afHeading="Vad gör jag på Fritiden?"
+            afHeading="Många personer gillar att koda"
             afHeadingLevel={InfoCardMultiHeadingLevel.H2}
             afType={InfoCardMultiType.RELATED}
             afLinkHref="/"
           >
-            <p>
-              På fritiden gillar jag att skapa spel, teckna, koda och baka
-              kakor!
-            </p>
+            <span>
+              Varför inte göra det till en karriär?
+            </span>
           </DigiInfoCardMulti>
         </DigiInfoCardMultiContainer>
       </DigiLayoutContainer>
