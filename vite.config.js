c83e080ce3f33cf3ef383bd67a1b043aed920732
mims <113519976+MimPMC@users.noreply.github.com>
2023-04-02 20:14:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      'text/javascript': ['js'],
-    },
-  },
+  // server: {
+  //   mimeTypes: {
+  //     'text/javascript': ['js'],
+  //   },
+  // },
 })
